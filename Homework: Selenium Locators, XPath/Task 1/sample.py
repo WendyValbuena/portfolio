03dc4aab6f91178aa7f5fd12ec8fa3d02bdb45b1
Ramo <ramovalbuena@gmail.com>
2024-12-18 15:31:28 +0100

    Create sample.py

---
+++ Homework: Selenium Locators, XPath/Task 1/sample.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver = webdriver.Chrome()
+
+try:
+    page_sample = ("file:///Users/ramonavalbuena/Documents/Pycharm/Selenium Locators, XPath/sample.html")
+    driver.get(page_sample)
+
+    input_field = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "inputField"))
+    )
+    print("ok")
+
+    input_field.send_keys("Hello world")
+
+    submit_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "submitButton"))
+    )
+
+    submit_button.click()
+    time.sleep(3)
+
+finally:
+
+    driver.quit()
